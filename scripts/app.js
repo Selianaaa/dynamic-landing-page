af9158e19f14246d2a5f001c9a6b263f294f2890
seliana <cany96@bk.ru>
2021-07-04 02:23:45 +0300

    feat: scroll content to start

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 const sectionsContent = [
   {
-    id: 'Home',
+    id: 'section1',
     titleTag: 'h1',
     title: 'Unmanned aerial vehicle',
     text: 'commonly known as a drone, is an aircraft without any human pilot, crew or passengers on board.',
@@ -8,7 +8,7 @@ const sectionsContent = [
   },
 
   {
-    id: 'Section2',
+    id: 'section2',
     titleTag: 'h2',
     title: 'UAV computing capability',
     text: 'followed the advances of computing technology, beginning with analog controls and evolving into microcontrollers, then system-on-a-chip (SOC) and single-board computers (SBC).',
@@ -16,7 +16,7 @@ const sectionsContent = [
   },
 
   {
-    id: 'Section3',
+    id: 'section3',
     titleTag: 'h2',
     title: 'The level of autonomy in UAVs varies widely.',
     text: 'UAV manufacturers often build in specific autonomous operations, such as: attitude stabilization on the pitch and roll axes, automatic roll and yaw control while moving horizontally.',
@@ -24,7 +24,7 @@ const sectionsContent = [
   },
 
   {
-    id: 'Section4',
+    id: 'section4',
     titleTag: 'h2',
     title: 'Applications',
     text: 'There are numerous civilian, commercial, military, and aerospace applications for UAVs. These include: Recreation, Disaster relief, archeology, search and rescue and traffic monitoring.',
@@ -33,99 +33,108 @@ const sectionsContent = [
 ]
 
 
-  /**
-   * Creates navbar links acording to sections amount
-   * @param {HTMLElement[]} sections - section elements
-   * @param {HTMLElement} navList - nav list element
-   */
-   const initializeData = (sections, navList) => {
-    sections.forEach((section) => {
-      const sectionTitle = section.getAttribute('data-section')
+/**
+ * Creates navbar links acording to sections amount
+ * @param {HTMLElement[]} sections - section elements
+ * @param {HTMLElement} navList - nav list element
+ */
+const initializeData = (sections, navList) => {
+  sections.forEach((section) => {
+    const sectionId = section.getAttribute('id')
 
-      const sectionContent = sectionsContent.find(
-        (content) => content.id === sectionTitle
-      )
- 
-      if (sectionContent) {
-        initializeNavItem(sectionTitle, navList)
+    const sectionContent = sectionsContent.find(
+      (content) => content.id === sectionId
+    )
 
-        initializeSection(section, sectionContent)
-      }
+    if (sectionContent) {
+      initializeNavItem(section, navList)
+      initializeSection(section, sectionContent)
+    }
 
-    })
-  }
+  })
+}
 
-   /**
-   * Create navbar item
-   * @param {string} sectionTitle - section title
-   * @param {HTMLElement} navList - nav list element
-   */
-  const initializeNavItem = (sectionTitle, navList) => {
-    const navItem = document.createElement('li')
-    navItem.textContent = sectionTitle
-    navItem.className = 'nav__link'
-    navItem.setAttribute('data-nav', sectionTitle)
+/**
+ * Create navbar item
+ * @param {HTMLElement} section - section element
+ * @param {HTMLElement} navList - nav list element
+ */
+const initializeNavItem = (section, navList) => {
+  const sectionTitle = section.getAttribute('data-section')
+  const navItem = document.createElement('li')
+  
+  navItem.textContent = sectionTitle
+  navItem.className = 'nav__link'
+  navItem.setAttribute('data-nav', sectionTitle)
 
-    navItem.addEventListener('click', () => {
-      section.scrollIntoView({behavior: 'smooth'})
-    })
+  navItem.addEventListener('click', () => {
+    section.scrollIntoView({behavior: 'smooth'})
+  })
 
-    navList.appendChild(navItem)
-  }
+  navList.appendChild(navItem)
+}
 
-  /**
-   * Create section content
-   * @param {HTMLElement} section - section element
-   * @param {object} sectionContent - section title
-   */
-  const initializeSection = (section, sectionContent) => {
-    const sectionContainer = section.querySelector('.section__container')
-    const sectionTitle = document.createElement(`${sectionContent.titleTag}`)
-    const sectionText = document.createElement('p')
+/**
+ * Create section content
+ * @param {HTMLElement} section - section element
+ * @param {object} sectionContent - section title
+ */
+const initializeSection = (section, sectionContent) => {
+  const sectionContainer = section.querySelector('.section__container')
+  const sectionTitle = document.createElement(`${sectionContent.titleTag}`)
+  const sectionText = document.createElement('p')
 
-    sectionTitle.textContent = sectionContent.title
-    sectionText.textContent = sectionContent.text
+  sectionTitle.textContent = sectionContent.title
+  sectionText.textContent = sectionContent.text
 
-    section.style.backgroundImage = `url('${sectionContent.image}')`
-    sectionContainer.appendChild(sectionTitle)
-    sectionContainer.appendChild(sectionText)
-  }
+  section.style.backgroundImage = `url('${sectionContent.image}')`
+  sectionContainer.appendChild(sectionTitle)
+  sectionContainer.appendChild(sectionText)
+}
 
+/**
+ * Check if section is visible
+ */
+const sectionIsViewed = (section) => {
+  const rect = section.getBoundingClientRect();
+  return rect.y <= 60 && rect.bottom >= 150
+}
 
-  /**
-   * Check if section is visible
-   */
-  const sectionIsViewed = (section) => {
-    const rect = section.getBoundingClientRect()
-    // console.log(section.getAttribute('data-section'), rect.top, rect.bottom, window.innerHeight)
-    return rect.top + 100 >= 0 && rect.bottom <= (window.innerHeight || document. documentElement.clientHeight)
-  }
+/**
+ * Scroll content to the begining
+ */
+ const scrollToTop = () => {
+  const topSection = document.getElementById(`${sectionsContent[0].id}`)
+  topSection.scrollIntoView({behavior: 'smooth'})
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const navList = document.getElementById('navbar__list')
   const sections = document.querySelectorAll('section')
+  const scrollTopElement = document.getElementById('scroll__top')
 
   window.addEventListener('scroll', () => {
-    sections.forEach((section) => {
+    
+    for (let section of sections) { 
       const sectionTitle = section.getAttribute('data-section')
       const sectionNavLink = navList.querySelector(`[data-nav='${sectionTitle}']`)
 
       if (sectionIsViewed(section)) {
         section.classList.add('active-section')
         sectionNavLink.classList.add('active-nav')
+        scrollTopElement.style.display = section.getAttribute('id') === sectionsContent[0].id ? 'none' : 'flex'
       } else {
         section.classList.remove('active-section')
         sectionNavLink.classList.remove('active-nav')
       }
-    })
+    }
   })
   
   initializeData(sections, navList)
 })
 
 
-// Add class 'active' to section when near top of viewport
 
 // Build menu 
 
