ae5ef2108d179e38ad69db67d9987a96e200a680
seliana <cany96@bk.ru>
2021-07-05 22:24:15 +0300

    feat: color accessibility

--- scripts/app.js
+++ scripts/app.js
@@ -87,7 +87,7 @@ const initializeSection = (section, sectionContent) => {
   sectionTitle.textContent = sectionContent.title
   sectionText.textContent = sectionContent.text
 
-  section.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('${sectionContent.image}')`
+  section.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${sectionContent.image}')`
   sectionContainer.appendChild(sectionTitle)
   sectionContainer.appendChild(sectionText)
 }
