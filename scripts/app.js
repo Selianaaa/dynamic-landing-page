00f8ca6473f68c50e075b23efecbec124c6bdf2d
seliana <cany96@bk.ru>
2022-01-30 22:17:42 +0300

    refactor: functions

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ const sectionsContent = [
     titleTag: 'h1',
     title: 'Unmanned aerial vehicle',
     text: 'commonly known as a drone, is an aircraft without any human pilot, crew or passengers on board.',
-    image: 'images/main.jpg'
+    image: 'images/main.jpg',
   },
 
   {
@@ -12,7 +12,7 @@ const sectionsContent = [
     titleTag: 'h2',
     title: 'UAV computing capability',
     text: 'followed the advances of computing technology, beginning with analog controls and evolving into microcontrollers, then system-on-a-chip (SOC) and single-board computers (SBC).',
-    image: 'images/titan.jpeg'
+    image: 'images/titan.jpeg',
   },
 
   {
@@ -20,7 +20,7 @@ const sectionsContent = [
     titleTag: 'h2',
     title: 'The level of autonomy in UAVs varies widely.',
     text: 'UAV manufacturers often build in specific autonomous operations, such as: attitude stabilization on the pitch and roll axes, automatic roll and yaw control while moving horizontally.',
-    image: 'images/tyber.jpeg'
+    image: 'images/tyber.jpeg',
   },
 
   {
@@ -28,9 +28,9 @@ const sectionsContent = [
     titleTag: 'h2',
     title: 'Applications',
     text: 'There are numerous civilian, commercial, military, and aerospace applications for UAVs. These include: Recreation, Disaster relief, archeology, search and rescue and traffic monitoring.',
-    image: 'images/aquila.jpg'
+    image: 'images/aquila.jpg',
   },
-]
+];
 
 /**
  * Creates navbar links according to sections amount
@@ -39,18 +39,18 @@ const sectionsContent = [
  */
 const initializeData = (sections, navList) => {
   sections.forEach((section) => {
-    const sectionId = section.getAttribute('id')
+    const sectionId = section.getAttribute('id');
 
     const sectionContent = sectionsContent.find(
       (content) => content.id === sectionId
-    )
+    );
 
     if (sectionContent) {
-      initializeNavItem(section, navList)
-      initializeSection(section, sectionContent)
+      initializeNavItem(section, navList);
+      initializeSection(section, sectionContent);
     }
-  })
-}
+  });
+};
 
 /**
  * Create navbar item
@@ -58,19 +58,19 @@ const initializeData = (sections, navList) => {
  * @param {HTMLElement} navList - nav list element
  */
 const initializeNavItem = (section, navList) => {
-  const sectionTitle = section.getAttribute('data-section')
-  const navItem = document.createElement('li')
-  
-  navItem.textContent = sectionTitle
-  navItem.className = 'nav__link'
-  navItem.setAttribute('data-nav', sectionTitle)
+  const sectionTitle = section.getAttribute('data-section');
+  const navItem = document.createElement('li');
+
+  navItem.textContent = sectionTitle;
+  navItem.className = 'nav__link';
+  navItem.setAttribute('data-nav', sectionTitle);
 
   navItem.addEventListener('click', () => {
-    section.scrollIntoView({behavior: 'smooth'})
-  })
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
 
-  navList.appendChild(navItem)
-}
+  navList.appendChild(navItem);
+};
 
 /**
  * Create section content
@@ -78,19 +78,19 @@ const initializeNavItem = (section, navList) => {
  * @param {object} sectionContent - section title
  */
 const initializeSection = (section, sectionContent) => {
-  const sectionContainer = section.querySelector('.section__container')
-  const sectionTitle = document.createElement(`${sectionContent.titleTag}`)
-  const sectionText = document.createElement('p')
+  const sectionContainer = section.querySelector('.section__container');
+  const sectionTitle = document.createElement(`${sectionContent.titleTag}`);
+  const sectionText = document.createElement('p');
 
-  sectionTitle.textContent = sectionContent.title
-  sectionText.textContent = sectionContent.text
+  sectionTitle.textContent = sectionContent.title;
+  sectionText.textContent = sectionContent.text;
 
   section.style.backgroundImage = `
     linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('${sectionContent.image}')
-  `
-  sectionContainer.appendChild(sectionTitle)
-  sectionContainer.appendChild(sectionText)
-}
+  `;
+  sectionContainer.appendChild(sectionTitle);
+  sectionContainer.appendChild(sectionText);
+};
 
 /**
  * Check if section is visible in viewport
@@ -100,66 +100,72 @@ const initializeSection = (section, sectionContent) => {
 const sectionIsVisible = (section, navbarHeight) => {
   const sectionRect = section.getBoundingClientRect();
 
-  return sectionRect.y <= navbarHeight && sectionRect.bottom >= navbarHeight
-}
+  return sectionRect.y <= navbarHeight && sectionRect.bottom >= navbarHeight;
+};
 
 /**
  * Scroll content to the begining
  */
- const scrollToTop = () => {
-  const topSection = document.getElementById(`${sectionsContent[0].id}`)
+const scrollToTop = () => {
+  const topSection = document.getElementById(`${sectionsContent[0].id}`);
 
-  topSection.scrollIntoView({behavior: 'smooth'})
-}
+  topSection.scrollIntoView({ behavior: 'smooth' });
+};
 
 /**
  * Check if menu is visible
  * @param {string} transform - current menu transform
  */
 const menuIsActive = (transform) => {
-  return transform === 'translateX(-180px)'
-}
+  return transform === 'translateX(-180px)';
+};
 
 /**
  * Handle mobile menu click
  */
- const handleMenuClick = () => {
-  const navbarButton = document.querySelector('.mobile__navbar__button')
-  const navbarList = document.getElementById('navbar__list')
-  const currentListTransform = navbarList.style.transform   
+const handleMenuClick = () => {
+  const navbarButton = document.querySelector('.mobile__navbar__button');
+  const navbarList = document.getElementById('navbar__list');
+  const currentListTransform = navbarList.style.transform;
 
-  navbarList.style.transform = menuIsActive(currentListTransform) 
-    ? 'translateX(180px)' 
-    : 'translateX(-180px)'
+  navbarList.style.transform = menuIsActive(currentListTransform)
+    ? 'translateX(180px)'
+    : 'translateX(-180px)';
 
-  navbarButton.classList.toggle('active__menu')
-}
+  navbarButton.classList.toggle('active__menu');
+};
 
 /**
  * Execute when DOM is loaded
  */
 document.addEventListener('DOMContentLoaded', () => {
-  const navbarMenu = document.querySelector('.navbar__menu')
-  const navList = document.getElementById('navbar__list')
-  const sections = document.querySelectorAll('section')
-  const scrollTopElement = document.getElementById('scroll__top')
+  const navbarMenu = document.querySelector('.navbar__menu');
+  const navList = document.getElementById('navbar__list');
+  const sections = document.querySelectorAll('section');
+  const scrollTopElement = document.getElementById('scroll__top');
 
   window.addEventListener('scroll', () => {
-    
-    for (let section of sections) { 
-      const sectionTitle = section.getAttribute('data-section')
-      const sectionNavLink = navList.querySelector(`[data-nav='${sectionTitle}']`)
+    for (let section of sections) {
+      const sectionTitle = section.getAttribute('data-section');
+      const sectionNavLink = navList.querySelector(
+        `[data-nav='${sectionTitle}']`
+      );
 
-      if (sectionIsVisible(section, navbarMenu.getBoundingClientRect().height)) {
-        section.classList.add('active-section')
-        sectionNavLink.classList.add('active-nav')
-        scrollTopElement.style.display = section.getAttribute('id') === sectionsContent[0].id ? 'none' : 'flex'
+      if (
+        sectionIsVisible(section, navbarMenu.getBoundingClientRect().height)
+      ) {
+        section.classList.add('active-section');
+        sectionNavLink.classList.add('active-nav');
+        scrollTopElement.style.display =
+          section.getAttribute('id') === sectionsContent[0].id
+            ? 'none'
+            : 'flex';
       } else {
-        section.classList.remove('active-section')
-        sectionNavLink.classList.remove('active-nav')
+        section.classList.remove('active-section');
+        sectionNavLink.classList.remove('active-nav');
       }
     }
-  })
-  
-  initializeData(sections, navList)
-})
+  });
+
+  initializeData(sections, navList);
+});
